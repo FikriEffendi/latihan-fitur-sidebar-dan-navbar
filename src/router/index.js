cccb8291d0be1928d3f965374c3ddad56c2b0fd7
aerochops <150325960+aerochops@users.noreply.github.com>
2024-10-30 22:15:56 +0700

    menyeting route

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import LayoutApp from "@/layouts/layout-app.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      components: LayoutApp,
+      children: [
+        {
+          path: "/",
+          name: "homeView",
+          component: HomeView,
+        },
+      ],
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  ],
+});
 
-export default router
+export default router;
